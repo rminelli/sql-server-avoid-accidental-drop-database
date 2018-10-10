a42ff2a7f68d447100419edadee8ff19a08a1386
Roberto Minelli <rminelli@gmail.com>
2018-10-10 16:54:42 -0300

    Initial commit

---
+++ databases-maintenance.sql
@@ -0,0 +1,29 @@
+/*
+	Script to avoid accidental deletion of database
+*/
+
+-- Trigger 
+
+CREATE TRIGGER Trig_prevention_Drop_Database 
+	ON ALL SERVER 
+
+FOR DROP_DATABASE
+
+AS
+
+RAISERROR('######### The DROP DATABASE function is disabled for this server! #########', 16,1);
+
+ROLLBACK;
+
+
+-- Disable Trigger
+DISABLE TRIGGER Trig_Prevencao_Drop_Database ON ALL SERVER
+
+
+-- Enable Trigger
+ENABLE TRIGGER Trig_Prevencao_Drop_Database ON ALL SERVER
+
+
+-- Drop Database
+DROP DATABASE -- Nome do banco
+GO
